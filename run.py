176383201204de79a2f572ce4d4b7acf21646e2e
Daniel Gilkarov <dgilkarov33@gmail.com>
2023-01-11 07:40:00 +0200

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ import sys
 app.config['DEBUG'] = True
 
 if __name__ == '__main__':
-    print(sys.argv[0])
-    app.run(host='0.0.0.0', port = int(sys.argv[0]))
+    print(sys.argv[1])
+    app.run(host='0.0.0.0', port = int(sys.argv[1]))
