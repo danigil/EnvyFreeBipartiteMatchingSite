225e3e204d64e96029e7e8ea30557e9aa8e7c828
Daniel Gilkarov <dgilkarov33@gmail.com>
2023-01-11 18:14:16 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from website_code import app
 
-import logging
-logging.basicConfig(level=logging.DEBUG)
+# import logging
+# logging.basicConfig(level=logging.DEBUG)
 
-if __name__ == '__main__':
-  app.run(debug=True, host="0.0.0.0", port=25656)
+# if __name__ == '__main__':
+#   app.run(debug=True, host="0.0.0.0", port=25656)
